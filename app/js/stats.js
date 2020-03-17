a81892a66dbd24e3739cae5774b2a559241319e8
vjrj <vjrj@ourproject.org>
2020-03-17 18:38:23 +0100

    Improved stats

--- app/js/stats.js
+++ app/js/stats.js
@@ -1,44 +1,52 @@
+var settings = require('./settings');
 var { locale } = require('./i18n_init');
-var gbifesjs = require('./settings');
+var { CountUp } = require('countup.js');
 
-function loadStats(){
+var collectory = settings.services.collectory.url;
+var biocacheService = settings.services.biocacheService.url;
 
-  $.getJSON("https://registros-ws.gbif.es/occurrences/search?pageSize=0", function( data ) {
-    $('#number_registros').html(addPeriods(data.totalRecords)).addClass('loaded_stats');
-  });
-
-  $.getJSON("https://colecciones.gbif.es/ws/institution", function( data ) {
-    $('#number_instituciones').html(data.length).addClass('loaded_stats');
-  });
-
-  $.getJSON("https://colecciones.gbif.es/ws/dataResource", function( data ) {
-    $('#number_bases').html(data.length).addClass('loaded_stats');
-  });
-
-  $.getJSON("https://registros-ws.gbif.es/occurrence/facets?q=*:*&facets=species&pageSize=0", function(data) {
-    $("#number_species").html(addPeriods(data[0].count)).addClass('loaded_stats');
-  });
-}
-
-
-function addPeriods(nStr)
-{
-  nStr += '';
-  var sep = locale === 'en' ? ',': '.';
-  var x = nStr.split('.');
-  var x1 = x[0];
-  var x2 = x.length > 1 ? '.' + x[1] : '';
-
-  var rgx = /(\d+)(\d{3})/;
-  while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, '$1' + sep + '$2');
+var setCounter = (id, val, onEnd) => {
+  const options = {
+    separator: locale === 'en' ? ',': '.',
+    duration: 1
+  };
+  // If testing set some dummy value
+  if (val === 0 && settings.isDevel) {
+    val = 123456;
   }
-  return x1 + x2;
+  options.startVal = Math.round(val - val * 4/100); // we increment only a %
+  console.log(`Start val ${options.startVal} to ${val}`);
+  const countUp = new CountUp(id, val, options);
+  if (!countUp.error) {
+    countUp.start(() => { $(`#${id}`).addClass('loaded_stats'); if (typeof onEnd !== 'undefined') onEnd(); });
+  } else {
+    console.error(countUp.error);
+  }
+};
+
+// If you want to show collections stats:
+// `${collectory}/ws/dataResource/count`
+
+var loadStats = () => {
+  $.getJSON(`${biocacheService}/occurrences`, (data) => {
+    setCounter('number_registros', data.totalRecords, () =>
+      $.getJSON(`${collectory}/ws/dataResource/count`, (data) => {
+        setCounter('number_bases', data.total, () =>
+          $.getJSON(`${collectory}/ws/institution/count`, (data) => {
+            setCounter('number_instituciones', data.total);
+          })
+        )
+      })
+    )});
+  // Right now this is slow so we put here
+  $.getJSON(`${biocacheService}/occurrence/facets?q=*:*&facets=species&pageSize=0`, (data) => {
+    setCounter("number_species", data[0].count);
+  });
 }
 
-document.addEventListener("DOMContentLoaded",function(){
-  if (document.location.host === 'datos.gbif.es' || document.location.host === 'localhost:3333'
-      || gbifesjs.isDevel ) {
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.location.host === 'datos.gbif.es' || document.location.host === 'demo.gbif.es' || document.location.host === 'localhost:3333'
+      || settings.isDevel ) {
     loadStats();
   }
 });
