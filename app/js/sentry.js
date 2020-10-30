b52faffcd6fa8ae96747689a1bc097fe2b3908cc
vjrj <vjrj@ourproject.org>
2020-10-30 14:41:47 +0100

    If sentry is not loaded or blocked don't fail

--- app/js/sentry.js
+++ app/js/sentry.js
@@ -1,37 +1,40 @@
-var gbifesjs = require('./settings');
-var { locale } = require('./i18n_init');
-var Cookies = require('js-cookie');
+var gbifesjs = require("./settings");
+var { locale } = require("./i18n_init");
+var Cookies = require("js-cookie");
 
-var user = Cookies.get('ALA-Auth');
+var user = Cookies.get("ALA-Auth");
 
-if (!gbifesjs.isDevel) {
-  Sentry.init({ dsn: gbifesjs.sentryUrl });
-  console.log('Sentry configured');}
+if (Sentry != null) {
+  if (!gbifesjs.isDevel) {
+    Sentry.init({ dsn: gbifesjs.sentryUrl });
+    console.log("Sentry configured");
+  }
 
+  var user = Cookies.get("ALA-Auth", { domain: "gbif.es", path: "/" });
 
-var user = Cookies.get('ALA-Auth', { domain: 'gbif.es', path: '/' });
+  if (typeof user !== "undefined") {
+    Sentry.configureScope(function (scope) {
+      scope.setUser({ email: user });
+      console.log("Sentry setting email scope");
+    });
+  } else {
+    console.log("Cannot set sentry email scope");
+  }
 
-if (typeof user !== 'undefined') {
-  Sentry.configureScope(function(scope) {
-    scope.setUser({"email": user});
-    console.log('Sentry setting email scope');
-  })
+  Sentry.configureScope(function (scope) {
+    scope.setTag("page_locale", locale);
+    console.log("Sentry setting lang scope");
+  });
+
+  const buildInfo = "$_BUILD";
+
+  console.log(`Build info: ${buildInfo}`);
+
+  Sentry.configureScope(function (scope) {
+    scope.setExtra("gbif-es-build", buildInfo);
+  });
 } else {
-  console.log('Cannot set sentry email scope');
+  console.log("Cannot use sentry, maybe is blocked");
 }
-
-Sentry.configureScope(function(scope) {
-  scope.setTag("page_locale", locale);
-  console.log('Sentry setting lang scope');
-});
-
-const buildInfo = '$_BUILD';
-
-console.log(`Build info: ${buildInfo}`);
-
-Sentry.configureScope(function(scope) {
-  scope.setExtra("gbif-es-build", buildInfo);
-});
-
 
 // Sentry.captureMessage('Pro pro probando probando');
