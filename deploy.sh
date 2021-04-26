b9d9d574231f7c47a964471974a8c3d1b0bd87ae
vjrj <vjrj@ourproject.org>
2021-04-26 12:30:05 +0200

    Clean cache

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,14 @@ brunch build --production
 rsync -a --delete public/ datos.gbif.es:/srv/auth.gbif.es/www/brand-2020-brunch/
 rsync -a public/ datos.gbif.es:/srv/auth.gbif.es/www/
 cp app/js/settings-demo.js app/js/settings.js
+
+curl -s https://registros.gbif.es/headerFooter/clearCache && \
+curl -s https://especies.gbif.es/headerFooter/clearCache && \
+curl -s https://listas.gbif.es/headerFooter/clearCache && \
+curl -s https://imagenes.gbif.es/headerFooter/clearCache && \
+curl -s https://doi.gbif.es/headerFooter/clearCache && \
+curl -s https://alertas.gbif.es/headerFooter/clearCache && \
+curl -s https://regiones.gbif.es/headerFooter/clearCache && \
+curl -s https://auth.gbif.es/userdetails/headerFooter/clearCache && \
+curl -s https://colecciones.gbif.es/headerFooter/clearCache
+
